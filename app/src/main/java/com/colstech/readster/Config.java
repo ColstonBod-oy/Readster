edc443cb41bdf28839b98cfeae4139fb35dbee08
ColstonBod-oy <bodoycolstond@gmail.com>
2025-10-31 13:19:40 +0800

    init

---
+++ app/src/main/java/com/colstech/readster/Config.java
@@ -0,0 +1,12 @@
+package com.colstech.readster;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+public class Config {
+
+	public static final int TESS_ENGINE = TessBaseAPI.OEM_LSTM_ONLY;
+
+	public static final String TESS_LANG = "eng";
+
+	public static final String IMAGE_NAME = "sample.jpg";
+}

ddb4137e11378b29d6efa48bb16099b95a3d5a89
ColstonBod-oy <bodoycolstond@gmail.com>
2025-10-31 13:28:48 +0800

    added material 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.google.android.material:material:1.12.0")
     implementation("cz.adaptech.tesseract4android:tesseract4android:4.9.0")
 }

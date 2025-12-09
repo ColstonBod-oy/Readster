f2a9eb14522e6d89da62fa840c3074d64ba2faaa
ColstonBod-oy <bodoycolstond@gmail.com>
2025-12-09 16:24:58 +0800

    updated capture to not save image

--- app/src/main/java/com/colstech/readster/MainActivity.java
+++ app/src/main/java/com/colstech/readster/MainActivity.java
@@ -9,8 +9,12 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.colstech.readster.ui.main.MainFragment;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,31 +26,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Only launch the camera if this is the first creation of the activity
+        // and we don't have a saved instance state (which would imply a recreation)
+        // AND we haven't already set the fragment (which might happen if we handle this differently).
+        // However, since we want to launch camera on start, but not loop:
+
         if (savedInstanceState == null) {
-            takePicture();
+            // Check if we already have a fragment showing, if so, don't launch camera again.
+            if (getSupportFragmentManager().findFragmentById(R.id.container) == null) {
+                takePicture();
+            }
         }
     }
 
     public void takePicture() {
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "New Picture");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
-        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            if (photoFile != null) {
+                imageUri = FileProvider.getUriForFile(this,
+                        "com.colstech.readster.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "temp_image";
+        File storageDir = getExternalCacheDir();
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return image;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance(imageUri))
-                    .commitNow();
-        } else {
-            Toast.makeText(this, "Image capture cancelled", Toast.LENGTH_SHORT).show();
-            finish();
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, MainFragment.newInstance(imageUri))
+                        .commitNow();
+            } else {
+                // If the user cancelled the camera, we should probably just close the activity
+                // or let them try again. But "goes back to initial landing screen" suggests
+                // maybe the HomeActivity is underneath?
+                Toast.makeText(this, "Image capture cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         }
     }
 }
